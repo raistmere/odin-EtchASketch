bc9a6739a9e7e0c72adcfea27947888f91c8e33b
raistmere <raistmere@gmail.com>
2023-05-24 18:37:51 -0500

    Added a button to generate new grid sizes on page
    New button allows the user to input a grid size through a prompt and
    generate a new grid based on that input.

--- js/script.js
+++ js/script.js
@@ -1,27 +1,38 @@
-//We grab the input from the user to figure out how big they want the grid to be.
-const userInput = prompt("Number of squares for the grid:");
+const generateButton = document.querySelector(".generateButton");
+console.log(generateButton);
+generateButton.addEventListener("click", generateGrid);
 
-//Because it's a grid, it's userInput by userInput.
-const totalSquares = userInput * userInput;
-
-//Go ahead and place all the divs under the gridContainer. The div count is based off totalSquares.
-const gridContainer = document.querySelector(".gridContainer");
-for (let i = 0; i < totalSquares; i++) 
+function generateGrid()
 {
-    //Create the new child div
-    let child = document.createElement('div');
-    child.className = "gridBox";
+    //We grab the input from the user to figure out how big they want the grid to be.
+    const userInput = prompt("Number of squares for the grid:");
 
-    //Each square on the grid will be pre calculated based on the userInput #.
-    let gridSize = (960 / userInput); //The grid will always be 960px wide
-    child.style.width = `${gridSize}px`;
-    child.style.height = `${gridSize}px`;
+    //Because it's a grid, it's userInput by userInput.
+    const totalSquares = userInput * userInput;
 
+    //Go ahead and place all the divs under the gridContainer. The div count is based off totalSquares.
+    const gridContainer = document.querySelector(".gridContainer");
 
-    child.addEventListener("mouseenter", function(e)
-    {
-        child.classList.add("hover");
-    });
+    //We clear all the divs in the gridContainer so we can add the correct divs based on userInput.
+    while(gridContainer.firstChild) gridContainer.removeChild(gridContainer.firstChild);
     
-    gridContainer.appendChild(child);
+    for (let i = 0; i < totalSquares; i++) 
+    {
+        //Create the new child div
+        let child = document.createElement('div');
+        child.className = "gridBox";
+
+        //Each square on the grid will be pre calculated based on the userInput #.
+        let gridSize = (960 / userInput); //The grid will always be 960px wide
+        child.style.width = `${gridSize}px`;
+        child.style.height = `${gridSize}px`;
+
+
+        child.addEventListener("mouseenter", function(e)
+        {
+            child.classList.add("hover");
+        });
+
+        gridContainer.appendChild(child);
+    }
 }
