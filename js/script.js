3a10578db4e6da23764e85af7b76b1b59aada599
raistmere <raistmere@gmail.com>
2023-05-24 17:48:25 -0500

    Added grid creation based on userInput from prompt
    Grid can now be created by user input from a prompt. The user can pick
    any size of grid.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,24 @@
+//We grab the input from the user to figure out how big they want the grid to be.
+const userInput = prompt("Number of squares for the grid:");
+
+//Because it's a grid, it's userInput by userInput.
+const totalSquares = userInput * userInput;
+
+//Go ahead and place all the divs under the gridContainer. The div count is based off totalSquares.
 const gridContainer = document.querySelector(".gridContainer");
-for (let i = 0; i < 256; i++) 
+for (let i = 0; i < totalSquares; i++) 
 {
+    //Create the new child div
     let child = document.createElement('div');
     child.className = "gridBox";
-    child.textContent = "Child";
 
-    child.addEventListener("mouseover", function(e)
+    //Each square on the grid will be pre calculated based on the userInput #.
+    let gridSize = (960 / userInput); //The grid will always be 960px wide
+    child.style.width = `${gridSize}px`;
+    child.style.height = `${gridSize}px`;
+
+
+    child.addEventListener("mouseenter", function(e)
     {
         child.classList.add("hover");
     });
